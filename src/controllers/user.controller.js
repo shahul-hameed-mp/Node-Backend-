d0e1245b8e311f95ff81bc33c6f77823dff63f37
shahul-hameed-mp <shahulkaniyambetta@gmail.com>
2025-08-31 16:01:40 +0530

    fix logoutUser: change refreshToken update method to use $unset for better clarity

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -127,7 +127,7 @@ const loginUser = asyncHandler(async (req,res) =>{
 });
 
 const logoutUser = asyncHandler(async (req,res) =>{
-    await User.findByIdAndUpdate(req.user._id, { $set: { refreshToken: undefined } }, { new: true });
+    await User.findByIdAndUpdate(req.user._id, { $unset: { refreshToken: 1 } }, { new: true });
      const options ={
         httpOnly: true,
         secure: true
