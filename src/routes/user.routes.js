f6e01d4b65b0d4c355b35e9c0cbdea43c61f427f
shahul-hameed-mp <shahulkaniyambetta@gmail.com>
2025-08-31 15:44:14 +0530

    implement watch history retrieval: add functionality to fetch user's watch history with video owner details

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,10 @@
 import { Router } from "express";
-import { registerUser ,loginUser,logoutUser,refreshAccessToken} from "../controllers/user.controller.js";
+import { registerUser ,loginUser,logoutUser,refreshAccessToken, changeCurrentPassword, getCurrentUser, 
+    updateUserDetails,
+    updateUserAvatar,
+    updateUserCoverImage,
+    getUserChannelProfile,
+    getWatchHistory} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJwt } from "../middlewares/auth.middleware.js";
 
@@ -12,5 +17,12 @@ router.post("/register",
 router.post("/login", loginUser);
 router.post("/logout", verifyJwt, logoutUser);
 router.post("/refresh-token", refreshAccessToken);
+router.post("/change-password", verifyJwt, changeCurrentPassword);
+router.get("/current-user", verifyJwt, getCurrentUser);
+router.patch("/update-account", verifyJwt, updateUserDetails);
+router.patch("/update-avatar", verifyJwt, upload.single("avatar"), updateUserAvatar);
+router.patch("/update-cover-image", verifyJwt, upload.single("coverImage"), updateUserCoverImage);
+router.get("/c/:username",verifyJwt,getUserChannelProfile)
+router.get("/history", verifyJwt, getWatchHistory);
 
 export default router;
